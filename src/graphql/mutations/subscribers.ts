1c4f3735259a94442686bb9b6c24035dbfe4b2d2
Joao Silva <joao.devweb@gmail.com>
2022-06-23 21:31:09 -0300

    feat(project): lesson 04 finished :rocket:

---
+++ src/graphql/mutations/subscribers.ts
@@ -0,0 +1,9 @@
+import { gql } from '@apollo/client';
+
+export const MUTATION_SUBSCRIBER = gql`
+  mutation CreateSubscriber($name: String!, $email: String!) {
+    createSubscriber(data: { name: $name, email: $email }) {
+      id
+    }
+  }
+`;
